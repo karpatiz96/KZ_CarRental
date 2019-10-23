dd77b3b37cd2aeb78667d62b4753cd56dd1e515d
Kárpáti Zoltán <karpati.z@upcmail.hu>
2019-10-23 18:50:10 +0200

    Confirm account, forgott password corrected.

---
+++ CarRental/CarRental.Bll/Dtos/ButtonConfirmationDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental.Bll.Dtos
+{
+    public class ButtonConfirmationDto
+    {
+        public ButtonConfirmationDto(string text, string url)
+        {
+            Text = text;
+            Url = url;
+        }
+
+        public string Text { get; set; }
+
+        public string Url { get; set; }
+    }
+}
