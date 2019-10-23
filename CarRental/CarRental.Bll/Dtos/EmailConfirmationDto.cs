29b564aeb5fade3ffa3561c49ec7ad190e0fdc5d
Kárpáti Zoltán <karpati.z@upcmail.hu>
2019-10-23 17:46:26 +0200

    Email confirmation, password forgot added.

---
+++ CarRental/CarRental.Bll/Dtos/EmailConfirmationDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRental.Bll.Dtos
+{
+    public class EmailConfirmationDto
+    {
+        public EmailConfirmationDto(string name, string url)
+        {
+            Name = name;
+            Url = url;
+        }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+    }
+}
